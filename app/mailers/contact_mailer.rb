0f73c7e30b3d2ad90e4f9c48606f2c3d613c8c98
Jimmy Comodeca <jimmy.comodeca@gmail.com>
2020-04-07 02:11:33 +0000

    Fixed contacts controller and contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to 'jimmy.comodeca@gmail.com'
+  default to: 'jimmy.comodeca@gmail.com'
   
   def contact_email(name, email, body)
    @name = name
